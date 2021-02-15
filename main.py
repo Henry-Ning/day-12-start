664d13b37d3ef9074ec5c32a00d31c4c65fd126a
Henry Ning <henryhuning@gmail.com>
2021-02-15 10:08:14 +0000

    116. How to Modify a Global Variable 5min

--- main.py
+++ main.py
@@ -4,10 +4,19 @@ enemies = 1
 player_health = 10
 
 def increase_enemies():
-  enemies = 2
+  enemies = 2 #This line does not change the global variable
+  print(f"enemies inside function: {enemies}")
+
+def add_enemies():
+  global enemies #This is how to change global variable
+  enemies +=1
   print(f"enemies inside function: {enemies}")
 
 increase_enemies()
+print(f"enemies outside function: {enemies}")
+
+add_enemies()
+
 print(f"enemies outside function: {enemies}")
 
 # If statement does not create local scope. Def does. Variables created under if statements are global variables. 
